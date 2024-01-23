9d690561585f2cc89fc77959fbe516be9ac89a42
Yasin <kurtulusyasin78@gmail.com>
2024-01-23 18:53:31 +0300

    First Commit

---
+++ Csharp_Giris/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+    public class Kurs
+    {
+        public string kursAdi { get; set; }
+        public string kursVeren { get; set; }
+        public int izlenmeYuzdesi { get; set; }
+
+    }
+}
---
+++ Csharp_Giris/Csharp_Giris/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Csharp_Giris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string kurs1 = "java kursu";
+            string kurs2 = "phyton kursu";
+            string kurs3 = "C kursu";
+            string kurs4 = "C # kursu";
+            string[] kurslar = new string[4];
+            kurslar[0] = kurs1;
+            kurslar[1] = kurs2;
+            kurslar[2] = kurs3;
+            kurslar[3] = kurs4;
+            foreach (string i in kurslar)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
